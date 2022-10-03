8d36b121daf4f2c957d32dac83182f09d22fad60
saras singh <saras.k.singh@gmail.com>
2022-10-03 12:25:20 +0530

    Initial commit

---
+++ JunitTesting/src.main.java/com/sks/Calculation.java
@@ -0,0 +1,18 @@
+package com.sks;
+
+public class Calculation {
+
+	public static double convertToDecimal(int numerator, int denominator) {
+		if (denominator == 0) {
+			throw new IllegalArgumentException("Denominator must not be 0");
+		}
+		return (double) numerator / (double) denominator;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hi : " + convertToDecimal(10, 2));
+		
+	}
+
+}
